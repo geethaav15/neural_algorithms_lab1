6f78f8f76449c6dd0fb0560d74763b957b6589cf
Bama <chorse@gmail.com>
2018-10-12 15:50:45 +0530

    Initial set of codes

---
+++ iris.py
@@ -0,0 +1,28 @@
+import csv
+training = []
+with open('iris.csv', 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        training = training+[row]
+#conversion of data-sets to a list
+training = training[:-1] # truncated the last one
+#print training
+print "Data details"
+print "Total samples", len(training)
+#Conversion of "Iris-Sesots" to 1 and "Iris-virginica" to -1"
+for sample in training:
+    if sample[4] =="Iris-setosa":
+        sample[4] = 1
+    else:
+        sample[4] = -1
+        
+#Truncate X and d values
+X = []
+for sample in training:
+    X = X+[sample[:4]]
+#print X, len(X)
+    
+d = []
+for sample in training:
+    d = d+[sample[-1]]
+#print d, len(d)
---
+++ iris_classify_perceptron.py
@@ -0,0 +1,52 @@
+import numpy as np
+import iris
+
+#print iris.X
+#print iris.d
+
+X = iris.X
+X = np.array(X)
+print "Input values"
+print X
+
+d = iris.d
+d = np.array(d)
+print "Teacher values"
+print d
+print "length", len(d)
+
+print "initial weights"
+w = np.zeros(4)
+print w
+c = 0.1
+
+y=X.astype(np.float)
+print y.dtype
+
+for n in range(1,5):
+    for i, x in enumerate(y):
+        net = np.dot(y[i],w) 
+        if net > 0:
+            out = 1
+        else:
+            out = -1
+        r = c*(d[i] - out)
+        delta_w = r*x
+        w = delta_w+w
+    print n, i, w
+final_out = []
+print "Testing"
+for i,x in enumerate(y):
+    net = np.dot(y[i],w)
+    if net>0:
+        out = 1
+    else:
+        out = -1
+    final_out = final_out+[out]
+print "final output", final_out
+print "d", d
+
+
+
+
+
