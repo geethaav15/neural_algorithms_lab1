6027316fa471957a74614826911bbea9b95e42de
geethaav15 <107907805+geethaav15@users.noreply.github.com>
2022-06-23 22:42:48 +0530

    Update mc_and.py

--- mc_and.py
+++ mc_and.py
@@ -46,3 +46,31 @@ ans3 = McAnd(x2,w,t)
 print (x2, "value", ans3)
 ans4 = McAnd(x3,w,t)
 print (x3, "value", ans4)
+
+### Testing MP-OR
+o0 = np.array([0,0,1])
+o1 = np.array([0,1,1])
+o2 = np.array([1,0,1])
+o3 = np.array([1,1,1])
+ow = np.array([2,2,-1])
+
+t = 0
+ans1 = McOR(o0,ow,t)
+print (x0, "value", ans1)
+ans2 = McOR(o1,ow,t)
+print (x1, "value", ans2)
+ans3 = McOR(o2,ow,t)
+print (x2, "value", ans3)
+ans4 = McOR(o3,ow,t)
+print (x3, "value", ans4)
+
+### Testing MP-NOT
+n0 = np.array([0,1])
+n1 = np.array([1,1])
+w = np.array([-1,1])
+
+t = 0
+ans1 = McNot(n0,w,t)
+print (x0, "value", ans1)
+ans2 = McNot(n1,w,t)
+print (x1, "value", ans2)
